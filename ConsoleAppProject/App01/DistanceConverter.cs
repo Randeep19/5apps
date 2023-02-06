02a88ed2a99864b0ca4d6007f345b1dddacab175
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2023-02-06 21:42:38 +0000

    adding feet and converting the feet to miles and then adding miles and converting the miles to feet

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,34 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public void feetToMiles()
+        {
+            //step 1- input the feet
+            Console.WriteLine("Enter the number of Feet:");
+            string input = Console.ReadLine();
+            int feet = Convert.ToInt32(input);
+
+            //step 2- convert the nuber of feet into miles
+            int miles = feet / 5280;
+            Console.WriteLine(feet + " feet is " + miles + " miles ");
+
+            //output the screen
+            Console.WriteLine(feet + " feet is " + miles.ToString("0.00") + " miles ");
+        }
+
+        public void milesToFeet()
+        {
+            //step 1- input the miles
+            Console.WriteLine("Enter the number of miles:");
+            string input = Console.ReadLine();
+            int miles = Convert.ToInt32(input);
+
+            //step 2- convert the nuber of miles into feet
+            int feet = miles * 5280;
+            Console.WriteLine(miles + " miles are " + feet + " feet ");
+
+            //output the screen
+            Console.WriteLine(miles + " miles are " + feet + " feet ");
+        }
     }
 }
