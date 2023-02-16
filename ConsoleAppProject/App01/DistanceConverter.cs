3e0755993f77335bab07f257e039efe662582cf4
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2023-02-16 17:39:38 +0000

    the methods MilesToFeet & FeetToMiles are removed.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -35,43 +35,7 @@ namespace ConsoleAppProject.App01
             ConvertDistance();
             print();
         }
-     
-  
-
-        public void MilesToFeet()
-        {
-            //step 1- input the miles
-            Console.WriteLine("Please enter the number of miles: ");
-            string input = Console.ReadLine();
-            miles = Convert.ToDouble(input);
-            Console.WriteLine("Miles Entered is: " + miles);
-
-            //step 2- convert the number of miles into feet
-            const int MILES_TO_FEET = 5280;
-            feet = miles * MILES_TO_FEET;
-            Console.WriteLine(miles + " miles are " + feet + " feet ");
-
-            //output the screen
-            Console.WriteLine(miles + " miles are " + feet + " feet ");
-        }
-        public void FeetToMiles()
-        {
-            //step 1- input the feet
-            Console.WriteLine("Enter the number of Feet:");
-            string input = Console.ReadLine();
-            feet = Convert.ToDouble(input);
-            Console.WriteLine("feet Entered is: " + feet);
-
-
-            //step 2- convert the number of feet into miles
-            const int MILES_TO_FEET = 5280;
-            miles = feet / MILES_TO_FEET;
-            Console.WriteLine(feet + " feet is " + miles + " miles ");
-
-            //output the screen
-            Console.WriteLine(feet + " feet is " + miles.ToString("0.00") + " miles ");
-        }
-
+   
 
 
         public void Input()
